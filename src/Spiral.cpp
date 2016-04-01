be79684540b755f93607294706b58864f6125220
BhargavaRavaliKoganti <BhargavaRavaliKoganti@missionrnd.com>
2016-04-01 13:05:00 +0530

    Submit Commit

--- src/Spiral.cpp
+++ src/Spiral.cpp
@@ -33,8 +33,50 @@ Note : Check the function Parameters ,Its a double pointer .
 
 #include "stdafx.h"
 #include<stdlib.h>
-
+void assignToArray(int, int, int, int, int**, int*, int*);
 int *spiral(int rows, int columns, int **input_array)
 {
-	return NULL;
+	if (rows <= 0 || columns <= 0 || input_array == NULL)
+		return NULL;
+	int i = 0;
+	int *index = &i;
+	int *array = (int*)malloc(sizeof(int)*(rows*columns));
+	assignToArray(0, rows, 0, columns, input_array, index, array);
+	return array;
 }
+
+void assignToArray(int rowstart, int rows, int columnstart, int columns, int **input_array, int *index, int *array){
+	if(rowstart > rows - 1)
+		return;
+	if(columnstart > columns - 1)
+		return;
+	for(int i = columnstart; i < columns; i++)
+	{
+		array[*index] = input_array[rowstart][i];
+		(*index)++;
+	}
+	
+	for(int i = rowstart + 1; i < rows; i++)
+	{
+		array[*index] = input_array[i][columns-1];
+		(*index)++;
+	}
+	if(columnstart != columns-1)
+		for(int i = columns-2; i >= columnstart; i--)
+		{
+			array[*index] = input_array[rows-1][i];
+			(*index)++;
+		}
+	if(rowstart != rows) 
+		for(int i = rows-2; i > rowstart; i--)
+		{
+			array[*index] = input_array[i][columnstart];
+			(*index)++;
+		}
+	assignToArray(rowstart+1, rows-1, columnstart+1, columns-1, input_array, index, array);
+	return;
+}
+
+
+
+
